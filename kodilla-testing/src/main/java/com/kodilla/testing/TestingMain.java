0102e69b07f8d2df7b0b7d3e81976de43d6ff5be
Przemek <p.bydolek@interia.pl>
2019-01-19 11:38:51 +0100

    met

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int a = 4;
+        int b = 3;
+        int resultAdd = a+b;
+        int resultSubtract = a-b;
+
+        Calculator calculator = new Calculator();
+
+        if(calculator.add(a, b)==resultAdd){
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(calculator.subtract(a, b)==resultSubtract){
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
