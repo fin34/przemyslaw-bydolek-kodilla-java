28eeb22e43167a0013d97f1f903a0413bc0d8a60
Przemek <p.bydolek@interia.pl>
2019-02-05 21:38:16 +0100

    met

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,18 +9,16 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile(){
+    public void readFile() throws FileReaderException{
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
-        try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+        try(Stream<String> fileLines = Files.lines(Paths.get("tttt/hn.txt"))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-
-            System.out.println("coś poszło do dupy!!!" + e);
+            throw new FileReaderException();
         } finally {
-
             System.out.println("Wykonam się zawsze!!!");
         }
     }
