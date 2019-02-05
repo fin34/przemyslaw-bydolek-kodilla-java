2760264869ec1be9c6a8222ba8e70d6d2bc28a2b
Przemek <p.bydolek@interia.pl>
2019-02-05 22:30:29 +0100

    met

--- kodilla-exception/src/main/java/com/kodilla/exception/test/MainFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/MainFlight.java
@@ -5,9 +5,9 @@ public class MainFlight {
     public static void main(String[] args) {
 
         Flight firstFlight = new Flight("Torino", "Rome");
-        FindFilghtLogic findFilght = new FindFilghtLogic();
+        FindFilghtLogic searchFilght = new FindFilghtLogic();
         try {
-            System.out.println("Connection found? " + findFilght.findFilght(firstFlight));
+            System.out.println("Connection found? " + searchFilght.findFilght(firstFlight));
         }catch (RouteNotFoundException e) {
             System.out.println("Airport not found");
         }
