fbcc0efff136efb0e20fb122eb3ba50d36795ba0
fin34 <p.bydolek@interia.pl>
2019-02-25 22:00:18 +0100

    task 9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -1,15 +1,17 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MovieStore movieStore = new MovieStore();
+        MovieStore films = new MovieStore();
 
+        String allFilms = films.getMovies().entrySet().stream()
+                .flatMap(x -> x.getValue().stream())
+                .collect(Collectors.joining(" ! ", "", ""));
+
+        System.out.println(allFilms);
     }
 }
