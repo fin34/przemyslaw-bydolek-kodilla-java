7eefcfe450b92bff3cf86643d34ba24679753983
fin34 <p.bydolek@interia.pl>
2019-03-12 22:09:28 +0100

    tasks 11

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigmac() {
+
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .Bun("Sesame")
+                .Burgers(2)
+                .Sauce("barbecue")
+                .Ingredient("onion")
+                .Ingredient("bacon")
+                .build();
+        System.out.println(bigmac);
+
+        int howManyIngredients = bigmac.getIngredients().size();
+        Assert.assertEquals(2, howManyIngredients);
+    }
+}
