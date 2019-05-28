83179ba989b2ce5f06b089b592f209a58e41a8a7
fin34 <p.bydolek@interia.pl>
2019-05-28 21:11:11 +0200

    task 20.2

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.patterns2.decorator.pizza;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.logging.SocketHandler;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,30 +11,50 @@ public class PizzaOrderTestSuite {
     @Test
     public void testOrderPizzaGetCost() {
         //Given
-        Order order = new BasicPizza();
-        order = new Cheese(order);
-        order = new Ham(order);
-        order = new Mushrooms(order);
-        order = new Tuna(order);
-        System.out.println(order.getCost());
+        Order firstOrder = new BasicPizza();
+        Order secondOrder = new BasicPizza();
+        Order thirdOrder = new BasicPizza();
+        firstOrder = new Cheese(firstOrder);
+        firstOrder = new Ham(firstOrder);
+        firstOrder = new Mushrooms(firstOrder);
+        firstOrder = new Tuna(firstOrder);
+        secondOrder = new Ham(secondOrder);
+        secondOrder = new Mushrooms(secondOrder);
+        System.out.println("Cost of first order: " + firstOrder.getCost());
+        System.out.println("Cost of second order: " + secondOrder.getCost());
+        System.out.println("Cost of third order: " + thirdOrder.getCost());
         //When
-        BigDecimal theCost = order.getCost();
+        BigDecimal costFirstOrder = firstOrder.getCost();
+        BigDecimal costSecondOrder = secondOrder.getCost();
+        BigDecimal costThirdOrder = thirdOrder.getCost();
         //Then
-        assertEquals(new BigDecimal(35), theCost);
+        assertEquals(new BigDecimal(35), costFirstOrder);
+        assertEquals(new BigDecimal(25), costSecondOrder);
+        assertEquals(new BigDecimal(15), costThirdOrder);
     }
 
     @Test
     public void testOrderPizzaGetDescription() {
         //Given
-        Order order = new BasicPizza();
-        order = new Cheese(order);
-        order = new Ham(order);
-        order = new Mushrooms(order);
-        order = new Tuna(order);
-        System.out.println(order.getDescription());
+        Order firstOrder = new BasicPizza();
+        Order secondOrder = new BasicPizza();
+        Order thirdOrder = new BasicPizza();
+        firstOrder = new Cheese(firstOrder);
+        firstOrder = new Ham(firstOrder);
+        firstOrder = new Mushrooms(firstOrder);
+        firstOrder = new Tuna(firstOrder);
+        secondOrder = new Ham(secondOrder);
+        secondOrder = new Mushrooms(secondOrder);
+        System.out.println("First order: " + firstOrder.getDescription());
+        System.out.println("Second order: " + secondOrder.getDescription());
+        System.out.println("Third order: " + thirdOrder.getDescription());
         //When
-        String description = order.getDescription();
+        String descriptionFirstOrder = firstOrder.getDescription();
+        String descriptionSecondOrder = secondOrder.getDescription();
+        String descriptionThirdOrder = thirdOrder.getDescription();
         //Then
-        assertEquals("Margherita + cheese + ham + mushrooms + tuna", description);
+        assertEquals("Margherita + cheese + ham + mushrooms + tuna", descriptionFirstOrder);
+        assertEquals("Margherita + ham + mushrooms", descriptionSecondOrder);
+        assertEquals("Margherita", descriptionThirdOrder);
     }
 }
